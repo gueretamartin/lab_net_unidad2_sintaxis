56eb0eb6f1079227029c42ccd8637010fbbe8c98
gueretamartin <guereta.martin@gmail.com>
2016-04-25 16:55:17 -0300

    Lab finalizado

--- clave/Program.cs
+++ clave/Program.cs
@@ -10,6 +10,27 @@ namespace clave
     {
         static void Main(string[] args)
         {
+
+           
+            string clave = "martin";
+            Boolean claveC = false;
+            Console.WriteLine("Introduzca la clave");
+            for( int intentos = 0; intentos < 4; intentos++) { 
+
+                string claveIntroducida = Console.ReadLine();
+                if (claveIntroducida == clave) {
+                    claveC = true;
+                    break;
+                }
+                Console.Clear();
+                Console.WriteLine("Vuelva a introducir la clave");
+            }
+            if (claveC == true)
+            {
+                Console.WriteLine("Clave Correcta");
+
+            } else { Console.WriteLine("Clave Incorrecta");  }
+            Console.ReadKey();
         }
     }
 }
